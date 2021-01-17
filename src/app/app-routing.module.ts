03c9ce2d7b2c9bdf6253f4854c42b0651ce972eb
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2021-01-16 23:37:31 -0500

    dependencias/rutas y componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RestartPasswordComponent } from './components/restart-password/restart-password.component';
+import { LoginComponent } from './components/login/login.component';
+import { EmitirNotaComponent } from './components/emitir-nota/emitir-nota.component';
+import { EmitirBoletaComponent } from './components/emitir-boleta/emitir-boleta.component';
+import { InformeBalanceComponent } from './components/informe-balance/informe-balance.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'restart-password', component: RestartPasswordComponent },
+  { path: 'emitir-nota', component: EmitirNotaComponent },
+  { path: 'emitir-boleta', component: EmitirBoletaComponent },
+  { path: 'informe-balance', component: InformeBalanceComponent } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,55 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Material
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatDialogModule} from '@angular/material/dialog';
+// My components
+import { LoginComponent } from './components/login/login.component';
+import { RestartPasswordComponent } from './components/restart-password/restart-password.component';
+import { EmitirNotaComponent } from './components/emitir-nota/emitir-nota.component';
+import { EmitirBoletaComponent } from './components/emitir-boleta/emitir-boleta.component';
+import { DetalleProductoModalComponent } from './components/detalle-producto-modal/detalle-producto-modal.component';
+import { InformeBalanceComponent } from './components/informe-balance/informe-balance.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RestartPasswordComponent,
+    EmitirNotaComponent,
+    EmitirBoletaComponent,
+    DetalleProductoModalComponent,
+    InformeBalanceComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatTableModule,
+    MatIconModule,
+    MatListModule,
+    MatDialogModule
+  ],
+  exports: [
     AppRoutingModule
+  ],
+  entryComponents: [
+    DetalleProductoModalComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
