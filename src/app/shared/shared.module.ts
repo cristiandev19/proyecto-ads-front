0895b5fccdb71e105c346eeae66b79fd2743aba5
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2021-01-24 16:27:59 -0500

    lo que sea

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+// My components
+import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
+
+@NgModule({
+  declarations: [
+    ConfirmModalComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatTableModule,
+    MatIconModule,
+    MatListModule,
+    MatDialogModule,
+    MatSidenavModule,
+    
+  ],
+  exports: [
+    ConfirmModalComponent
+  ],
+  entryComponents: [
+    ConfirmModalComponent
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class SharedModule { }
