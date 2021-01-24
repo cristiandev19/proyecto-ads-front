46ee7c1cc712c09e079e1d346f6148962fa8f8ba
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2021-01-24 16:27:43 -0500

    reporte inventario

---
+++ src/app/components/reporte-inventario/reporte-inventario.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reporte-inventario',
+  templateUrl: './reporte-inventario.component.html',
+  styleUrls: ['./reporte-inventario.component.scss']
+})
+export class ReporteInventarioComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { }
+}
