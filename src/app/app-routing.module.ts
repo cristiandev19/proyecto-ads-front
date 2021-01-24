f5649768d10def55b5baf5dd513670f0f6b30149
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2021-01-24 16:26:51 -0500

    agregando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,21 @@ import { LoginComponent } from './components/login/login.component';
 import { EmitirNotaComponent } from './components/emitir-nota/emitir-nota.component';
 import { EmitirBoletaComponent } from './components/emitir-boleta/emitir-boleta.component';
 import { InformeBalanceComponent } from './components/informe-balance/informe-balance.component';
+import { RegistrarReclamoComponent } from './components/registrar-reclamo/registrar-reclamo.component';
+import { CambiarBoletaComponent } from './components/cambiar-boleta/cambiar-boleta.component';
+import { GestionarUsuarioComponent } from './components/gestionar-usuario/gestionar-usuario.component';
+import { ReporteInventarioComponent } from './components/reporte-inventario/reporte-inventario.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'restart-password', component: RestartPasswordComponent },
   { path: 'emitir-nota', component: EmitirNotaComponent },
   { path: 'emitir-boleta', component: EmitirBoletaComponent },
-  { path: 'informe-balance', component: InformeBalanceComponent } 
+  { path: 'informe-balance', component: InformeBalanceComponent },
+  { path: 'registrar-reclamo', component: RegistrarReclamoComponent },
+  { path: 'cambiar-boleta', component: CambiarBoletaComponent },
+  { path: 'gestionar-usuario', component: GestionarUsuarioComponent },
+  { path: 'reporte-inventario', component: ReporteInventarioComponent }
 ];
 
 @NgModule({
