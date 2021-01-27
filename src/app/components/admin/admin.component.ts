47a8893b968082bf0380da8ef87b4d1fed83c085
cristiandev <cristiansotomayor.dev@gmail.com>
2021-01-27 01:45:29 -0500

    tengo que rehacer esto

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void { }
+
+  handleLogout() {
+    console.log('entrooo')
+    localStorage.removeItem('user');
+
+    this.router.navigate(['/auth/login'])
+  }
 }
