08df74b2835e7b5dc77114c9b4d8f47dd6aadd11
cristiandev <cristiansotomayor.dev@gmail.com>
2021-01-31 23:04:08 -0500

    home cambios

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IUser } from 'src/app/app.model';
+import { AppService } from 'src/app/app.service';
 
 
 @Component({
@@ -10,12 +11,21 @@ import { IUser } from 'src/app/app.model';
 })
 export class HomeComponent implements OnInit {
   user: IUser;
+  acciones: any = [];
 
   constructor(
-    private router: Router
+    private router: Router,
+    private appSrv: AppService
   ) {
     const user: string = localStorage.getItem('user') || '';
     this.user = JSON.parse(user);
+
+    this.appSrv.getAccionesXRol(this.user._id_rol).subscribe((res: any) => {
+      console.log('res', res);
+      this.acciones = res.acciones;
+    }, err => {
+      console.log('err', err);
+    })
   }
 
   ngOnInit(): void { }
