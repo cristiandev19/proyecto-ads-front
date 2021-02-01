fe34393e932165dbf39bb17a3b68db44278b7376
cristiandev <cristiansotomayor.dev@gmail.com>
2021-01-31 23:04:18 -0500

    detalle producto modal

--- src/app/components/detalle-producto-modal/detalle-producto-modal.component.ts
+++ src/app/components/detalle-producto-modal/detalle-producto-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { IProducto } from 'src/app/app.model';
 
@@ -32,17 +33,31 @@ const DETALLES = [
   styleUrls: ['./detalle-producto-modal.component.scss']
 })
 export class DetalleProductoModalComponent implements OnInit {
+  @Output() eventEmit = new EventEmitter();
   producto: IProducto;
   detalles: any = [];
+  cantidad: FormControl;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
-    console.log('data', data)
     this.producto = data.producto;
-    
+
+    this.cantidad = new FormControl('', [
+      Validators.required,
+      Validators.max(this.producto.stock),
+      Validators.pattern(/^\d+$/)
+    ]);
   }
 
 
   ngOnInit(): void { }
+
+  handleAgregar() {
+    const cantidad = this.cantidad.value;
+    this.eventEmit.emit({
+      ...this.producto,
+      cantidad: +cantidad
+    });
+  }
 }
